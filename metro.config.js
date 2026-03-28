89598cd9654b9c556b2a3c6a4168307cc00e8390
Aaron Zhang <aaronzhxng@gmail.com>
2026-03-27 23:44:24 -0500

    3/27/26/3
    changed metro.config.js so maybe the file can build

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,15 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.blockList = [/scripts\/.*/];
+
+config.resolver.extraNodeModules = {
+  "react-native-reanimated/scripts/validate-worklets-version": path.resolve(
+    __dirname,
+    "node_modules/react-native-reanimated/scripts/validate-worklets-version.js",
+  ),
+};
 
 module.exports = config;
