00b936c0b0b6d60a1259cee53a1bc7e97da0362e
Debarshi Kundu <debarshikundu0@utexas.edu>
2026-05-27 02:55:02 -0500

    UpdateToast readded

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { PolicyAreasProvider } from "./context/PolicyAreasContext";
 import { TabBarProvider } from "./context/TabBarContext";
+import UpdateToast from "./global_components/UpdateToast";
 import { initializeDatabase } from "./utils/database";
 import { storage } from "./utils/storage";
 import { syncListItemsFromBills } from "./utils/syncListItems";
@@ -145,6 +146,7 @@ export default function RootLayout() {
                 }}
               />{" "}
             </Stack>
+            <UpdateToast />
           </TabBarProvider>
         </PolicyAreasProvider>
       </QueryClientProvider>
